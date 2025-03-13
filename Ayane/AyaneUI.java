c737bb7526bc57a9d2c1f0e43a7524192ece899a
douglasaubre51 <boseallen192@gmail.com>
2025-03-13 15:23:11 +0530

    created ayane ui

---
+++ Ayane/AyaneUI.java
@@ -0,0 +1,11 @@
+import java.swing.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+
+public class AyaneUI{
+	public static void main(String[] args){
+		System.out.println("starting AyaneUI...");
+
+	}
+}
+
