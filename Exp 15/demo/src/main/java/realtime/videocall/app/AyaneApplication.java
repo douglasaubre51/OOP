a627b53a5b7e5067204635357072dcea7d8cde77
douglasaubre51 <boseallen192@gmail.com>
2025-03-13 14:13:22 +0530

    added exp 15

---
+++ Exp 15/demo/src/main/java/realtime/videocall/app/AyaneApplication.java
@@ -0,0 +1,13 @@
+package realtime.videocall.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyaneApplication.class, args);
+	}
+
+}
---
+++ Exp 15/demo/src/test/java/realtime/videocall/app/AyaneApplicationTests.java
@@ -0,0 +1,13 @@
+package realtime.videocall.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
