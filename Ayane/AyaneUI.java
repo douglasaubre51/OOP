4a31f5c7894b29c0f945a7925d42935e24440f38
douglasaubre51 <boseallen192@gmail.com>
2025-03-13 15:33:37 +0530

    ran ayane successfully!

--- Ayane/AyaneUI.java
+++ Ayane/AyaneUI.java
@@ -1,4 +1,4 @@
-import java.swing.*;
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
 
@@ -6,6 +6,13 @@ public class AyaneUI{
 	public static void main(String[] args){
 		System.out.println("starting AyaneUI...");
 
+		JFrame jFrame=new JFrame("Ayane - Realtime Video Calling App");
+
+		jFrame.setLayout(null);
+
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		jFrame.setSize(800,800);
+		jFrame.setVisible(true);
 	}
 }
-
