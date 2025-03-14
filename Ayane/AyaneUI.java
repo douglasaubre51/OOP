4173478e43540ea8d6f879434c83096cbb6dfed0
douglasaubre51 <boseallen192@gmail.com>
2025-03-14 10:49:27 +0530

    created a form in ayane ui

--- Ayane/AyaneUI.java
+++ Ayane/AyaneUI.java
@@ -27,23 +27,45 @@ public class AyaneUI {
 		jPanel.add(h1Label, h1gbc);
 
 		// name
-		JLabel nameLabel = new JLabel("Name");
-		GridBagConstraints nameLabelgbc = new GridBagConstraints();
-		nameLabelgbc.gridx = 0;
-		nameLabelgbc.gridy = 1;
+		// JLabel nameLabel = new JLabel("Name");
+		// GridBagConstraints nameLabelgbc = new GridBagConstraints();
+		// nameLabelgbc.gridx = 0;
+		// nameLabelgbc.gridy = 1;
 
-		jPanel.add(nameLabel, nameLabelgbc);
+		// jPanel.add(nameLabel, nameLabelgbc);
+
+		// name box
+		JTextField nameBox=new JTextField(15);
+		nameBox.setText("email");
+
+		GridBagConstraints nameBoxgbc=new GridBagConstraints();
+		nameBoxgbc.gridx=0;
+		nameBoxgbc.gridy=1;
+
+		jPanel.add(nameBox,nameBoxgbc);
+
+		// password box
+		JTextField passwordBox=new JTextField(15);
+		passwordBox.setText("password");
+
+		GridBagConstraints passwordBoxgbc=new GridBagConstraints();
+		passwordBoxgbc.gridx=0;
+		passwordBoxgbc.gridy=3;
+
+		jPanel.add(passwordBox,passwordBoxgbc);
 
 		// submit button
 		JButton submitBtn = new JButton("submit");
 
 		GridBagConstraints submitBtngbc = new GridBagConstraints();
 		submitBtngbc.gridx = 0;
-		submitBtngbc.gridy = 3;
+		submitBtngbc.gridy = 4;
 
 		jPanel.add(submitBtn, submitBtngbc);
 
 		// to show app
 		jFrame.setVisible(true);
+
+		System.out.println("ayane is running!...");
 	}
 }
