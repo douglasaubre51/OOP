f56912f364d718d4dafd27e2ac747e57ef313ea5
allen <boseallen192@gmail.com>
2025-03-14 22:22:08 +0530

    1 fixed hibernate 2 created table user

--- Ayane Repo/demo/demo/src/main/java/realtime/videocall/app/entities/User.java
+++ Ayane Repo/demo/demo/src/main/java/realtime/videocall/app/entities/User.java
@@ -1,8 +1,24 @@
 package realtime.videocall.app.entities;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false, unique = true)
+    private String emailID;
+
+    @Column
+    private boolean isOnline = false;
+
+    @Column
+    private boolean isOnCall = false;
 }
