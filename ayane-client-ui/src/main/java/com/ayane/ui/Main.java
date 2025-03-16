c6e2cacb4ec34718e2bc17a158778b8e4a36cb65
allen <boseallen192@gmail.com>
2025-03-16 16:47:52 +0530

    updated main enabled live capture!

--- ayane-client-ui/src/main/java/com/ayane/ui/Main.java
+++ ayane-client-ui/src/main/java/com/ayane/ui/Main.java
@@ -1,19 +1,30 @@
 package com.ayane.ui;
 
-import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+import org.bytedeco.javacv.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
 
-        VideoCapture camera = new VideoCapture(0);
+		try{
 
-        if (camera.isOpened())
-            System.out.println("JavaCv is working!");
+			OpenCVFrameGrabber grabber=new OpenCVFrameGrabber(0);
+			grabber.setFrameRate(60);
 
-        else
-            System.out.println("failure to access webcam!");
 
-        camera.release();
-    }
+			grabber.start();
+
+			System.out.println("video stream live!");
+
+			CanvasFrame frame=new CanvasFrame("web cam stream");
+
+			while(frame.isVisible()){
+				Frame capturedImage=grabber.grab();
+				frame.showImage(capturedImage);
+				System.out.println("displaying!");
+			}
+		}
+		catch(Exception e){ e.printStackTrace(); }
+
+	}
 }
