8f1a3289856b7fd0da0ceb6561a921da0c44ac81
allen <boseallen192@gmail.com>
2025-03-16 10:30:03 +0530

    1 created test for javacv

--- ayane-client-ui/src/main/java/com/ayane/ui/Main.java
+++ ayane-client-ui/src/main/java/com/ayane/ui/Main.java
@@ -1,7 +1,19 @@
 package com.ayane.ui;
 
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        VideoCapture camera = new VideoCapture(0);
+
+        if (camera.isOpened())
+            System.out.println("JavaCv is working!");
+
+        else
+            System.out.println("failure to access webcam!");
+
+        camera.release();
     }
 }
