a1c9699aeb38c7d44a0457b7355fed487bfc4ca3
allen <boseallen192@gmail.com>
2025-03-16 09:39:42 +0530

    1 removed ayane ui project 2 added ayane-client-ui maven project

---
+++ Ayane/webcamStreamDemo.java
@@ -0,0 +1,12 @@
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+
+public class webcamStreamDemo {
+    public static void main(String[] args) {
+        System.out.println("Starting webcam stream!");
+
+    }
+
+}
